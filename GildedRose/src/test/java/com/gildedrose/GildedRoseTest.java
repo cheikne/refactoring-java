4851d5f6e917697fb909f2145f451c9cb7efb0ee
kane <kane.cheickne@gmail.com>
2022-09-25 01:43:43 +0200

    The tests done on the variables Qaulity

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,7 +1,9 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+// import static org.hamcrest.MatcherAssert.assertEquals;
 import static org.hamcrest.Matchers.*;
 
 class GildedRoseTest {
@@ -13,6 +15,22 @@ class GildedRoseTest {
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertThat(element.name, is("foo"));
+  }
+  @Test
+  @DisplayName("Test that the value Quality becomes negative")
+  void TestQualityIsNegative(){
+    Item element = new Item("MaAXIME",0,20);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    Assertions.assertFalse(element.quality<0);
+  }
+  @Test
+   @DisplayName("Test that the final value of Quality is different at value actual")
+  void TestEqualityValueActualEndQuality(){
+    Item element = new Item("MaAXIME",0,20);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    Assertions.assertEquals(18,element.quality,()->"Les valeur sont differentes");
   }
 
 }
